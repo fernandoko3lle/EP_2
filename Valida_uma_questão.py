4f6b1b4ca0eb0d07834bdeb85f0e14bee72c5059
fernandoko3lle <fernandok3@al.insper.edu.br>
2022-10-26 22:19:02 -0300

    Quase la

--- Valida_uma_questão.py
+++ Valida_uma_questão.py
@@ -5,23 +5,27 @@ def valida_questao(dic_entrada):
             dic_saida[chave] = 'nao_encontrado'
     if len(dic_entrada.keys()) != 4:
         dic_saida['outro'] = 'numero_chaves_invalido'
-    if dic_entrada['titulo'].strip(' ') == '':
+    if dic_entrada['titulo'].strip() == '':
         dic_saida['titulo'] = 'vazio'
     if dic_entrada['nivel'] not in ('facil', 'medio', 'dificil'):
         dic_saida['nivel'] = 'valor_errado'
     if len(dic_entrada['opcoes'].keys()) == 4:
-        for opçao in dic_entrada['opcoes'].keys():
-            if opçao not in('A', 'B', 'C', 'D'):
-                dic_saida['opçoes'] = 'chave_invalida_ou_nao_encontrada'
-                if opçao == '' or opçao == ' ':
-                    dic_entrada['opcoes'][opçao] = 'vazia'  
+        for opçao, resposta in dic_entrada['opcoes'].items():
+            if opçao in('A', 'B', 'C', 'D'):
+                    if resposta.strip() == '':
+                        if 'opcoes' not in dic_saida:
+                            dic_saida['opcoes'] = {}
+                            dic_saida['opcoes'][opçao] = 'vazia'
+                        else:
+                            dic_saida['opcoes'][opçao] = 'vazia'
+            else:
+                dic_saida['opcoes'] = 'chave_invalida_ou_nao_encontrada'
     else: 
         dic_saida['opcoes'] = 'tamanho_invalido'
-    if 'correta' in dic_entrada and dic_entrada['correta'] not in ('A', 'B', 'C', 'D'):
-        dic_saida['correta'] = 'valor_errado'
+    if 'correta' not in dic_saida:
+        if dic_entrada['correta'] not in ('A', 'B', 'C', 'D'):
+            dic_saida['correta'] = 'valor_errado'
     return dic_saida
-
-
         
 
         
