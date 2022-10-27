abc883d62708697cdd35ac95c39820c5eb043437
fernandoko3lle <fernandok3@al.insper.edu.br>
2022-10-27 07:35:16 -0300

    organização

--- Valida_uma_questão.py
+++ Valida_uma_questão.py
@@ -1,34 +1,40 @@
-def valida_questao(dic_entrada): 
-    dic_saida = {}
+def valida_questao(Questão): 
+    Validação = {}
     for chave in ('titulo', 'nivel', 'opcoes', 'correta'):
-        if chave not in dic_entrada:
-            dic_saida[chave] = 'nao_encontrado'
-    if len(dic_entrada.keys()) != 4:
-        dic_saida['outro'] = 'numero_chaves_invalido'
-    if 'titulo' in dic_entrada: 
-        if dic_entrada['titulo'].strip() == '':
-            dic_saida['titulo'] = 'vazio'
-    if 'nivel' in dic_entrada:
-        if dic_entrada['nivel'] not in ('facil', 'medio', 'dificil'):
-            dic_saida['nivel'] = 'valor_errado'
-    if 'opcoes' in dic_entrada:
-        if len(dic_entrada['opcoes'].keys()) == 4:
-            for opçao, resposta in dic_entrada['opcoes'].items():
+        if chave not in Questão:
+            Validação[chave] = 'nao_encontrado'
+    if len(Questão.keys()) != 4:
+        Validação['outro'] = 'numero_chaves_invalido'
+    if 'titulo' in Questão: 
+        if Questão['titulo'].strip() == '':
+            Validação['titulo'] = 'vazio'
+    if 'nivel' in Questão:
+        if Questão['nivel'] not in ('facil', 'medio', 'dificil'):
+            Validação['nivel'] = 'valor_errado'
+    if 'opcoes' in Questão:
+        if len(Questão['opcoes'].keys()) == 4:
+            for opçao, resposta in Questão['opcoes'].items():
                 if opçao in('A', 'B', 'C', 'D'):
                         if resposta.strip() == '':
-                            if 'opcoes' not in dic_saida:
-                                dic_saida['opcoes'] = {}
-                                dic_saida['opcoes'][opçao] = 'vazia'
+                            if 'opcoes' not in Validação:
+                                Validação['opcoes'] = {}
+                                Validação['opcoes'][opçao] = 'vazia'
                             else:
-                                dic_saida['opcoes'][opçao] = 'vazia'
+                                Validação['opcoes'][opçao] = 'vazia'
                 else:
-                    dic_saida['opcoes'] = 'chave_invalida_ou_nao_encontrada'
+                    Validação['opcoes'] = 'chave_invalida_ou_nao_encontrada'
         else: 
-            dic_saida['opcoes'] = 'tamanho_invalido'
-    if 'correta' not in dic_saida:
-        if dic_entrada['correta'] not in ('A', 'B', 'C', 'D'):
-            dic_saida['correta'] = 'valor_errado'
-    return dic_saida
+            Validação['opcoes'] = 'tamanho_invalido'
+    if 'correta' not in Validação:
+        if Questão['correta'] not in ('A', 'B', 'C', 'D'):
+            Validação['correta'] = 'valor_errado'
+    return Validação
+
+print(valida_questao({
+  'titulo': 'Qual o resultado da operação 57 + 32?',
+  'nivel': 'moleza',
+  'opcoes': {'A': '  ', 'B': '85', 'C': '89', 'D': '\t'}
+}))
 
 
 
