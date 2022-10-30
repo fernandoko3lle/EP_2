3f23b78a33bf5ae3a6f0d8080056292e34bf0bf4
fernandoko3lle <fernandok3@al.insper.edu.br>
2022-10-30 18:42:09 -0300

    Dica

--- Gera_ajuda_em_uma_questao.py
+++ Gera_ajuda_em_uma_questao.py
@@ -1,33 +1,28 @@
 import random 
-
 def gera_ajuda(questao):
     repetidas = []
-    l_questoes =[]
+    l_respostas_erradas = []
+    dica = []
     correta = questao['correta']
     dic_questoes = questao['opcoes']
     del dic_questoes[correta]
     for r in dic_questoes.values():
-        l_questoes.append(r)
+        l_respostas_erradas.append(r)
     contagem = random.choice(range(2))
     contagem = contagem+1
     for a in range(contagem):
-      print(random.choice(l_questoes))
-        
+        dica.append(random.choice(l_respostas_erradas))
+    if len(dica) == 2:
+        if dica[0] == dica[1]:
+            del dica[1]
+    if len(dica) == 2:        
+        x = 'DICA:\nOpções certamente erradas: {0} | {1}'.format(dica[0], dica[1])
+    else:
+        x = 'DICA:\nOpções certamente erradas: {0}'.format(dica[0])
 
-        
-
-
-            
-
-        
-        
-        
-        
-
-
-
-
-gera_ajuda({
+    return x 
+    
+print(gera_ajuda({
   "titulo": "Qual destes parques não se localiza em São Paulo?!",
   "nivel": "facil",
   "opcoes": {
@@ -37,4 +32,4 @@ gera_ajuda({
     "D": "Morro da Urca"
   },
   "correta": "C"
-})
+}))
