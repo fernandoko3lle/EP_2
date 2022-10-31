4229adf5ed4806cd440cc635e11a336661be66bd
felipeap3 <felipeap3@al.insper.edu.br>
2022-10-31 11:08:19 -0300

    Introdução

--- jogo.py
+++ jogo.py
@@ -1,3 +1,14 @@
-input('Digite seu nome para começar: ')
-print('Bem vindo ao jogo da Fortuna de Dessoft')
-print('')
+nome = input('''Bem vindo ao Fortuna DesSoft,
+Digite seu nome para começar: ''')
+print('''
+Olá {0}, pronto para ser o mais novo milinonário?
+No Fortuna você terá que responder perguntas de multiplas escolha podendo ganhar até 1 milhão de reais!!!!
+Cada resposta correta aumentará seu premio!!!
+
+CUIDADO basta uma resposta incorreta para perder tudo.
+
+Durante o jogo você terá direito de pular 2 perguntas e solicitar 3 ajudas para os universitários.
+Para pular ou pedir ajuda digite: 'pula' ou 'ajuda'
+Você pode parar a qualquer momento do jogo, para isso digite: 'parar' 
+
+BOA SORTE, QUE A FORTUNA ESTEJA COM VOCE'''.format(nome))
